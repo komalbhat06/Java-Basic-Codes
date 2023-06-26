bcf30e9806b4f10ea9260a3439d36860f27d0c44
Komal Bhat <68904706+komalbhat06@users.noreply.github.com>
2023-06-26 15:05:55 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,12 @@
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String stringinput = "Komal";
+		 char[] resultarray = stringinput.toCharArray();
+		 for (int i = resultarray.length - 1; i >= 0; i--)
+		 System.out.print(resultarray[i]);
+
+}
+}
