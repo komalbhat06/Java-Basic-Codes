2020c6d3634c2cd0180130ad0a6b7126ee573c78
Komal Bhat <68904706+komalbhat06@users.noreply.github.com>
2023-06-26 14:40:18 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,16 @@
+
+public class factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int fact=1;
+		while(a>0) {
+			fact=fact*a;
+			a--;
+		}
+		System.out.println("factorial of  required number is: " + fact);
+
+	}
+
+}
